496c9278d03470f46e6c7f35a5cb92a3572584a9
TomCube <121310847+yomcube@users.noreply.github.com>
2023-05-23 07:49:35 -0500

    mark composite numbers

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-var primetable = document.getElementById("primetable");
+var primetable = document.getElementById('primetable');
 var max = 1000;
 function fillTable() {
-    var tableRow = document.createElement("tr");
+    var tableRow = document.createElement('tr');
     for (var i = 1; i <= max; i++) {
-        if (i % 10 == 1 && i != 1) {
-            tableRow = document.createElement("tr");
-        }
-        var tableCell = document.createElement("td");
-        if (!(i % 2 == 0 && i != 2)) tableCell.innerText = i;
+        var tableCell = document.createElement('td');
+        tableCell.innerText = i;
+        tableCell.id = i;
         tableRow.appendChild(tableCell);
         primetable.appendChild(tableRow);
     }
 }
+function markComposites() {
+    for (var i = 2; i <= max; i++) {
+        for (var n = 2; n <= max; n++) {
+            if (n % i == 0 && n != i) {
+                mark(n);
+            }
+        }
+    }
+}
+function mark(n) {
+    var elem = document.getElementById(n);
+    elem.class = 'composite';
+}
 fillTable();
+markComposites();
