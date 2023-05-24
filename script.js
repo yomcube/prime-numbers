2a82f1844d95570a4950de6a298186ca8e27b67f
TomCube <121310847+yomcube@users.noreply.github.com>
2023-05-23 21:53:34 -0500

    Fix bug where table was all one row

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var max = 1000;
 function fillTable() {
     var tableRow = document.createElement('tr');
     for (var i = 1; i <= max; i++) {
+        if (i % 10 == 1 && i != 1) {
+            tableRow = document.createElement("tr");
+        }
         var tableCell = document.createElement('td');
         tableCell.innerText = i;
         tableCell.id = i;
