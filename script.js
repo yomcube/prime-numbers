911b38483bc8a9b1f3f58a4f77d92543eda8a17d
TomCube <121310847+yomcube@users.noreply.github.com>
2023-05-25 07:33:00 -0500

    Fix errors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var primetable = document.getElementById("primetable");
 var max = 1000;
-function fillTable() {
+function fillTable(j) {
     var tableRow = document.createElement("tr");
-    for (var i = 1; i <= max; i++) {
+    for (var i = j; i <= max; i++) {
         if (i % 10 == 1 && i != 1) {
             tableRow = document.createElement("tr");
         }
@@ -13,9 +13,9 @@ function fillTable() {
         primetable.appendChild(tableRow);
     }
 }
-function markComposites() {
+function markComposites(j) {
     mark(1);
-    for (var i = 2; i <= max; i++) {
+    for (var i = j; i <= max; i++) {
         for (var n = 2; n <= max; n++) {
             if (n % i == 0 && n != i) {
                 mark(n);
@@ -27,5 +27,11 @@ function mark(n) {
     var elem = document.getElementById(n);
     elem.setAttribute("class", "composite");
 }
-fillTable();
-markComposites();
+function reTable() {
+    var oldmax = max;
+    max += 1000;
+    fillTable(max);
+    markComposites(max);
+}
+fillTable(1);
+markComposites(2);
